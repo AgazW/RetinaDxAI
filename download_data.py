15eb5372cd6943e1a510649b158bd5cf402f1605
Agaz Wani <AgazW@users.noreply.github.com>
2025-07-08 23:17:23 -0500

    Moved file to scr data folder

--- download_data.py
+++
@@ -1,8 +0,0 @@
-# Code to download data from Kaggle
-
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("abhinav099802/eye-disease-image-dataset")
-
-print("Path to dataset files:", path)
