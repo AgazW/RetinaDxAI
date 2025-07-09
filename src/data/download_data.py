7b9fac861e88490826f2ddc5815934455aec2c34
Agaz Wani <AgazW@users.noreply.github.com>
2025-07-08 23:23:42 -0500

    Create directory if doesn't exist

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,6 +1,13 @@
 # Code to download data from Kaggle
 
 import kagglehub
+import os
+
+# Specify your desired directory
+my_download_dir = "data/external"  # or any absolute path like "/home/user/data"
+
+# Create the directory if it doesn't exist
+os.makedirs(my_download_dir, exist_ok=True)
 
 # Download latest version
 path = kagglehub.dataset_download("abhinav099802/eye-disease-image-dataset")
