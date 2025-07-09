18b562ee86c771662a52fcd6b2b394a86b64713d
Agaz Wani <AgazW@users.noreply.github.com>
2025-07-09 01:01:03 -0500

    Copy download data to data directory

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -2,14 +2,18 @@
 
 import kagglehub
 import os
+import shutil
 
 # Specify your desired directory
-my_download_dir = "data/external"  # or any absolute path like "/home/user/data"
+target_dir = "../data/external"  # or any absolute path like "/home/user/data"
 
 # Create the directory if it doesn't exist
-os.makedirs(my_download_dir, exist_ok=True)
+os.makedirs(target_dir, exist_ok=True)
 
 # Download latest version
 path = kagglehub.dataset_download("abhinav099802/eye-disease-image-dataset")
+
+# Copy contents
+shutil.copytree(path, target_dir, dirs_exist_ok=True)
 
 print("Path to dataset files:", path)
