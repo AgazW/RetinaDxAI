640e58ef1d4e2f7b256ba260b49871b7eb098ebf
Agaz Wani <AgazW@users.noreply.github.com>
2025-07-20 15:54:43 -0500

    Add functions to save preprocessed image batches and entire dataset

--- notebooks/preprocessing.py
+++ notebooks/preprocessing.py
@@ -1,6 +1,8 @@
 
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import torch
+import os
 
 def get_transforms(img_size=(224, 224)):
     """
@@ -36,3 +38,34 @@ def get_image_dataloader(data_dir, batch_size=32, img_size=(224, 224), shuffle=T
     dataset = datasets.ImageFolder(root=data_dir, transform=transform)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
     return dataloader, dataset.classes, dataset.class_to_idx
+
+
+def save_preprocessed_batches(dataloader, save_dir):
+    """
+    Saves each batch of images and targets from the dataloader as separate .pt files.
+    Args:
+        dataloader: PyTorch DataLoader yielding (images, targets)
+        save_dir: Directory to save .pt files
+    """
+    os.makedirs(save_dir, exist_ok=True)
+    for i, (images, targets) in enumerate(dataloader):
+        batch_path = os.path.join(save_dir, f"batch_{i:04d}.pt")
+        torch.save({'images': images, 'targets': targets}, batch_path)
+
+
+
+def save_entire_dataset(dataloader, save_path):
+    """
+    Saves the entire dataset as a single .pt file containing all images and targets tensors.
+    Args:
+        dataloader: PyTorch DataLoader yielding (images, targets)
+        save_path: Path to save the .pt file
+    """
+    all_images = []
+    all_targets = []
+    for images, targets in dataloader:
+        all_images.append(images)
+        all_targets.append(targets)
+    images_tensor = torch.cat(all_images, dim=0)
+    targets_tensor = torch.cat(all_targets, dim=0)
+    torch.save({'images': images_tensor, 'targets': targets_tensor}, save_path)
