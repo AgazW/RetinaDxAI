882c917fe295065e3e3871e91deb719c38de1d6d
Agaz Wani <AgazW@users.noreply.github.com>
2025-07-20 11:26:31 -0500

    Refactor download_eye_disease_dataset function to improve docstring and parameter naming

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -4,16 +4,15 @@ import kagglehub
 import os
 import shutil
 
-def download_kaggle_dataset(
-		target_dir="../data/external", 
-		kaggle_dataset="abhinav099802/eye-disease-image-dataset"):
+def download_eye_disease_dataset(
+		target_dir = "../data/external",
+		kaggle_data = "abhinav099802/eye-disease-image-dataset"):
 	
 	"""
-	Downloads a dataset from Kaggle and copies it to the target directory.
+	Downloads the Eye Disease Image Dataset from Kaggle and copies it to the target directory.
 
 	Args:
 		target_dir (str): The directory where the dataset will be copied.
-		kaggle_dataset (str): The Kaggle dataset identifier (e.g., "owner/dataset-name").
 
 	Returns:
 		str: The path to the dataset files in the target directory.
@@ -21,12 +20,12 @@ def download_kaggle_dataset(
 	
 	os.makedirs(target_dir, exist_ok=True)
 	
+	try:
 		# Download latest version
-		path = kagglehub.dataset_download(kaggle_dataset)
+		path = kagglehub.dataset_download(kaggle_data)
 		# Copy contents
 		shutil.copytree(path, target_dir, dirs_exist_ok=True)
 		print("Path to dataset files:", target_dir)
-		return target_dir
 		return target_dir
 	
 	except Exception as e:
